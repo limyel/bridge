d490c3082be4fb7f33ffba2b585bb7511111a7a4
limyel <limyel@outlook.com>
2023-01-03 23:26:24 +0800

    补充注释

--- bridge-server/src/main/java/com/limyel/bridge/server/net/BridgeServer.java
+++ bridge-server/src/main/java/com/limyel/bridge/server/net/BridgeServer.java
@@ -15,19 +15,26 @@ public class BridgeServer {
     private Channel channel;
 
     public void bind(int port, ChannelInitializer<SocketChannel> channelInitializer) {
-        // 监听端口
+        // 1. 声明线程池
+        // 处理Accept事件
         NioEventLoopGroup bossGroup = new NioEventLoopGroup();
-        // 处理连接
+        // 处理读写事件
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
 
         try {
+            // 2. 创建服务端引导类
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             serverBootstrap
+                    // 3. 设置线程池
                     .group(bossGroup, workerGroup)
+                    // 4. 设置 ServerSocketChannel 类型
                     .channel(NioServerSocketChannel.class)
+                    // 5. 设置参数
                     .option(ChannelOption.SO_BACKLOG, 1024)
                     .childOption(ChannelOption.SO_KEEPALIVE, true)
                     .childOption(ChannelOption.TCP_NODELAY, true)
+                    // 6. 设置Handler（略）
+                    // 7. 设置子Handler
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
@@ -36,6 +43,7 @@ public class BridgeServer {
                     });
 
             channel = serverBootstrap
+                    // 8. 绑定端口
                     .bind(port)
                     .addListener(future -> {
                         if (future.isSuccess()) {
@@ -45,10 +53,13 @@ public class BridgeServer {
                             throw new RuntimeException();
                         }
                     }).sync().channel();
+
+            // 9. 等待服务端口关闭
             channel.closeFuture().addListener(future -> {
+                // 10. 关闭线程池
                 workerGroup.shutdownGracefully();
                 bossGroup.shutdownGracefully();
-            });
+            }).sync();
 
         } catch (InterruptedException e) {
             e.printStackTrace();
